2118ba5ab26544520d1d1daabd24d488043af085
Yaelglz-inf <nvgs502@gmail.com>
2026-06-14 22:08:38 -0600

    Configuracion de la logica del main activity

--- app/src/main/java/com/example/proyectoimc/MainActivity.kt
+++ app/src/main/java/com/example/proyectoimc/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.proyectoimc
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -21,5 +23,50 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.bttCalcular.setOnClickListener {
+            calcularIMC()
+        }
+
+
+        binding.bttResultados.setOnClickListener {
+            startActivity(Intent(this, Resultados::class.java))
+        }
+
+
     }
+
+    private fun calcularIMC() {
+        val nombre = binding.etNombre.text.toString()
+        val pesoStr = binding.etPeso.text.toString()
+        val estaturaStr = binding.etEstatura.text.toString()
+
+        if (nombre.isEmpty() || pesoStr.isEmpty() || estaturaStr.isEmpty()) {
+            Toast.makeText(this, "Llena todos los campos", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val peso = pesoStr.toDouble()
+        val estatura = estaturaStr.toDouble() / 100
+        val imc = peso / (estatura * estatura)
+
+        val categoria = when {
+            imc < 18.5 -> "Bajo peso"
+            imc < 25.0 -> "Peso normal"
+            imc < 30.0 -> "Sobrepeso"
+            else       -> "Obesidad"
+        }
+
+        val categoriaDescrip = when {
+            imc < 18.5 -> "${nombre} tu IMC indica que tu peso es inferior al rango considerado saludable, estar por debajo del peso recomendado puede dejar a tu cuerpo sin las reservas de energía necesarias y debilitar tu sistema inmunológico."
+            imc < 25.0 -> "¡Felicidades ${nombre}! Tu peso se encuentra dentro del rango ideal para tu estatura, esto indica un buen equilibrio entre la energía que consumes y la que gastas, el objetivo ahora es mantener este balance y enfocarte en tu salud a largo plazo."
+            imc < 30.0 -> "${nombre} tu IMC se encuentra en la categoría de sobrepeso, esto funciona como una señal de advertencia de tu cuerpo. No se trata de hacer dietas extremas, sino de realizar pequeños ajustes en tu rutina diaria que generen un gran impacto en tu energía y salud a futuro."
+            else       -> "${nombre} tu IMC indica obesidad, en este punto, el exceso de peso puede comenzar a elevar el riesgo de desarrollar condiciones como presión arterial alta, colesterol elevado o resistencia a la insulina."
+        }
+
+        binding.tvwIMcResultado.text = "IMC: ${"%.2f".format(imc)} — $categoria"
+
+        binding.tvwResultadoDescrp.text = "$categoriaDescrip"
+    }
+
+
 }
